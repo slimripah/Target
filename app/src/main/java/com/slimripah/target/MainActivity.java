ff6225793101f062c30fa03fda15b5fca06cd068
slimripah <slimripah@gmail.com>
2023-07-27 13:29:46 +0300

    create sign up page

--- app/src/main/java/com/slimripah/target/MainActivity.java
+++ app/src/main/java/com/slimripah/target/MainActivity.java
@@ -2,13 +2,27 @@ package com.slimripah.target;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
+
+    //variables
+    private static final int SPLASH_TIMER = 5000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(getApplicationContext(), SignUp.class);
+            startActivity(intent);
+            finish();
+        }, SPLASH_TIMER);
+
     }
 }
