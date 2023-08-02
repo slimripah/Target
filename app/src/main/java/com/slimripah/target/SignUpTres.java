1e51e663e98540dfc09b642a4a81d7a9a72ebc75
slimripah <slimripah@gmail.com>
2023-08-02 15:30:26 +0300

    sign up page tres

---
+++ app/src/main/java/com/slimripah/target/SignUpTres.java
@@ -0,0 +1,14 @@
+package com.slimripah.target;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUpTres extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_tres);
+    }
+}
