6d8e01562c4fad7db357528f750265be27e8531b
slimripah <slimripah@gmail.com>
2023-08-07 14:01:32 +0300

    sign up restructure

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,9 @@ dependencies {
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation ("com.airbnb.android:lottie:3.7.0")
+    implementation ("com.chaos.view:pinview:1.4.3")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
 }
