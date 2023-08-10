8e6e3b1ee5b9aa2c5d6f14d419a1e7559558c401
slimripah <slimripah@gmail.com>
2023-08-10 16:16:48 +0300

    sign up finish

--- app/src/main/java/com/slimripah/target/SignUpCinco.java
+++ app/src/main/java/com/slimripah/target/SignUpCinco.java
@@ -2,13 +2,28 @@ package com.slimripah.target;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
 public class SignUpCinco extends AppCompatActivity {
+
+    //variables
+    private static final int SPLASH_TIMER = 5000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_sign_up_cinco);
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(getApplicationContext(), Dashboard.class);
+            startActivity(intent);
+            finish();
+        }, SPLASH_TIMER);
+
     }
+    
 }
